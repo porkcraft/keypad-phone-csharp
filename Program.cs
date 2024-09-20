4b59455e6ca13ac26b7f278e1589a8e446e7a841
Minmin Khant <salaiminminkhant@gmail.com>
2024-09-20 15:25:18 +0700

    Added comments
    Added comments and minor variable name changes

--- Program.cs
+++ Program.cs
@@ -3,8 +3,9 @@
 
         string theOutput = "";
         char lastPressed = '\0';
-        int count = 0;
+        int pressCount = 0;
 
+        // Mapping of the keypad
         Dictionary<char, string> keypad = new Dictionary<char, string>() {
             { '2', "ABC" },
             { '3', "DEF" },
@@ -17,53 +18,58 @@
             { '0', " "}
         };
 
+        // Loop over each character in the input string
         foreach (char c in input) {
 
+            // If the character is a number between 2 & 9
             if (keypad.ContainsKey(c)) {
                 if (lastPressed == c)
                 {
-                    count++;
+                    pressCount++;
                 }
                 else {
                     if (lastPressed != '\0') {
                         string index = keypad[lastPressed];
-                        theOutput += index[(count - 1) % index.Length];
+                        theOutput += index[(pressCount - 1) % index.Length];
                     }
                     lastPressed = c;
-                    count = 1;
+                    pressCount = 1;
                 }
             }
+            // Handle the pause between letters
             else if (c == ' ') {
                 if (lastPressed != '\0') {
                     string index = keypad[lastPressed];
-                    theOutput += index[(count - 1) % index.Length];
+                    theOutput += index[(pressCount - 1) % index.Length];
                     lastPressed = '\0';
-                    count = 0;
+                    pressCount = 0;
                 }
             }
+            // Handle the Send button
             else if (c == '#') {
                 if (lastPressed != '\0') {
                     string index = keypad[lastPressed];
-                    theOutput += index[(count - 1) % index.Length];
+                    theOutput += index[(pressCount - 1) % index.Length];
                 }
                 break;
             }
+            // Handle the Backspace button
             else if (c == '*') {
                 if (theOutput.Length > 0) {
-                    theOutput = theOutput.Substring(0, theOutput.Length);
+                    theOutput = theOutput.Remove(theOutput.Length);
                 }
                 lastPressed = '\0';
-                count = 0;
+                pressCount = 0;
             }
         }
         return theOutput;
     }
 
+    // Testing of the examples
     static void Main(string[] args) {
         Console.WriteLine(OldPhonePad("33#"));
         Console.WriteLine(OldPhonePad("227*#"));
         Console.WriteLine(OldPhonePad("4433555 555666#"));
         Console.WriteLine(OldPhonePad("8 88777444666*664#"));
-        Console.WriteLine(OldPhonePad("77702#"));
     }
 }
